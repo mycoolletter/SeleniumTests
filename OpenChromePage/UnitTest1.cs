4f226b17ae1b3c1450acc1561864b576165f6bd2
mycoolletters <mycoolletters@gmail.com>
2021-01-05 14:15:22 +0200

    Add project files.

---
+++ OpenChromePage/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenChromePage
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int firstValue = 3;
+            int secondValue = 2;
+            Assert.IsTrue(5 == firstValue + secondValue);
+           
+        }
+    }
+}
